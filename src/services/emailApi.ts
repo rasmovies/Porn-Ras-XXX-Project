d53fe866c0e12077e697f9162106a4419636040a
Ras <rasmovies@proton.me>
2025-11-16 00:30:39 +0300

    Update emailApi.ts

--- src/services/emailApi.ts
+++ src/services/emailApi.ts
@@ -59,7 +59,7 @@ async function postJson<TInput extends object, TResponse>(path: string, body: TI
     const url = buildUrl(path);
     console.log('📤 POST request:', { url, path, body });
     
-    const response = await fetch ("https://vercel.com/ras-projects-6ebe5a01/porn-ras-xxx-project/9eshvpyrSuVE8fCcKe7ipBZrxRtd" {
+    const response = await fetch ('https://vercel.com/ras-projects-6ebe5a01/porn-ras-xxx-project/9eshvpyrSuVE8fCcKe7ipBZrxRtd', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
